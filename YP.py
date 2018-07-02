4c3c0af3ac9de6fe7fabd96c6f538cd1e851a217
Dmitry Ponyatov <dponiatov@icbcom.ru>
2018-07-02 16:05:27 +0400

    + Var
    Signed-off-by: Dmitry Ponyatov <dponiatov@icbcom.ru>

--- YP.py
+++ YP.py
@@ -1,9 +1,17 @@
 # http://yieldprolog.sourceforge.net/tutorial1.html
 
-def Persons():
-    yield 'Alpha'
-    yield 'Beta'
-    yield 'Gamma'
+class Var:
+    def __init__(self): self._value = None
+    def __lshift__(self,val): self._value = val
+    def __repr__(self):
+        if self._value: return '<%s>' % self._value
+        else: return str(self._value)
 
-for p in Persons():
+def Persons(P):
+    P << 'Alpha' ; yield P
+    P << 'Beta'  ; yield P
+    P << 'Gamma' ; yield P
+
+P = Var() ; print P
+for p in Persons(P):
     print p
