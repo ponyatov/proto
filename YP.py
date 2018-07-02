d491156a5176f1e43247d40b7a8dc7bf657f897d
Dmitry Ponyatov <dponiatov@icbcom.ru>
2018-07-02 18:00:35 +0400

    Signed-off-by: Dmitry Ponyatov <dponiatov@icbcom.ru>

--- YP.py
+++ YP.py
@@ -1,17 +1,29 @@
 # http://yieldprolog.sourceforge.net/tutorial1.html
 
-class Var:
-    def __init__(self): self._value = None
-    def __lshift__(self,val): self._value = val
+class UniVar:
+    def __init__(self):
+        self._value = None
+        self._bound = False
+    def __lshift__(self,val):
+        return self.unify(val)
+    def unify(self,arg):
+        if not self._bound:
+            self._value = arg ; self._bound = True
+            yield self
+            # drop binding on continue from here
+            self._value = None ; self._bound = False
+        elif self._value == arg:
+            # elso yield if bound to the same value
+            yield self
     def __repr__(self):
-        if self._value: return '<%s>' % self._value
+        if self._bound: return '<%s>' % self._value
         else: return str(self._value)
 
 def Persons(P):
-    P << 'Alpha' ; yield P
-    P << 'Beta'  ; yield P
-    P << 'Gamma' ; yield P
+    for i in P << 'Alpha': yield i
+    for i in P << 'Beta' : yield i
+    for i in P << 'Gamma': yield i
 
-P = Var() ; print P
+P = UniVar() ; print P
 for p in Persons(P):
     print p
