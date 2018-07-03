dbfc795a704aaedac67e2b717c7cafe244cb6719
Dmitry Ponyatov <dponiatov@icbcom.ru>
2018-07-03 16:56:03 +0400

    - PLY
    Signed-off-by: Dmitry Ponyatov <dponiatov@icbcom.ru>

--- PP/vm.py
+++ PP/vm.py
@@ -3,41 +3,7 @@ import sys
 ## data stack
 D = []
 
-# ## syntax parser by PLY (Python Lex-Yacc) @ http://www.dabeaz.com/ply/
-# 
-# import ply.lex as lex
-# 
-# ## supported tokens
-# tokens = ['WORD','NUMBER']
-# 
-# ## ignore spaces
-# t_ignore = ' \t\r'
-# 
-# ## line comments can start with # and \
-# ## block comments uses ( )
-# t_ignore_COMMENT = r'[\#\\].*|\(.*\)'
-# 
-# ## new line rule increments line counter in lexer
-# def t_newline(t):
-#     r'\n'
-#     t.lexer.lineno += 1
-#     
-# ## lexer error callback
-# def t_error(t):
-#     raise SyntaxError(t)
-# 
-# ## number parser rule
-# def t_NUMBER(t):
-#     r'[\+\-]?[0-9]+(\.[0-9]*)?'
-#     return t
-# 
-# ## any other non-space groups treated as FORTH word names
-# def t_WORD(t):
-#     r'[A-Za-z0-9_]+'
-#     return t    
-# 
-# ## single lexer instance
-# lexer = lex.lex()
+## syntax parser by RPLY @ http://rply.readthedocs.io/en/latest/
 
 from rply import LexerGenerator
 
@@ -50,15 +16,14 @@ lexer_generator.add('SPACE',r'\s+')
 lexer = lexer_generator.build()
 
 def WORD(source):
-    try: token = stream.next() ; D.append(token) ; return token
+    try: token = source.next() ; D.append(token) ; return token
     except StopIteration: return None
 
 def INTERPRET(SRC=''):
-#     lexer.input(SRC)            # feed lexer with source code
-    source = lexer.lex('1 2.3')
+    source = lexer.lex('1 2.3')         # feed lexer with source code
     while True:
-        if not WORD(source): break    # break on end of source
-        print D
+        if not WORD(source): break      # break on end of source
+        print D.pop()
 
 def main(argv):
     print argv
