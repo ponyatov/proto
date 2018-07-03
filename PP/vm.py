b714106312ad0c72ec4b5cc1f4cded65195146cd
Dmitry Ponyatov <dponiatov@icbcom.ru>
2018-07-03 17:58:20 +0400

    Signed-off-by: Dmitry Ponyatov <dponiatov@icbcom.ru>

--- PP/vm.py
+++ PP/vm.py
@@ -4,25 +4,36 @@ import sys
 D = []
 
 ## syntax parser by RPLY @ http://rply.readthedocs.io/en/latest/
-
 from rply import LexerGenerator
 
+## generator will build lexer in runtime
 lexer_generator = LexerGenerator()
 
+## drop comments
+lexer_generator.ignore(r'[\\\#].*|\(.*\)')
+## drop spaces
+lexer_generator.ignore(r'\s+')
+## number parsing
+lexer_generator.add('HEX','0x[0-9A-Fa-f]+')
+lexer_generator.add('BIN','0b[01]+')
 lexer_generator.add('NUMBER',r'[\+\-]?[0-9]+(\.[0-9]*)?')
+## FORTH word names
 lexer_generator.add('WORD',r'[A-Za-z0-9_]+')
-lexer_generator.add('SPACE',r'\s+')
 
+## build resulting lexer
 lexer = lexer_generator.build()
 
+## parse next token from
+## @param[in] source stream
 def WORD(source):
     try: token = source.next() ; D.append(token) ; return token
     except StopIteration: return None
 
+## REPL loop
 def INTERPRET(SRC=''):
-    source = lexer.lex('1 2.3')         # feed lexer with source code
+    source = lexer.lex(SRC)         # feed lexer with source code
     while True:
-        if not WORD(source): break      # break on end of source
+        if not WORD(source): break  # break on end of source
         print D.pop()
 
 def main(argv):
