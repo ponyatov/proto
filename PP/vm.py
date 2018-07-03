91492d4c10761cb84eaf0a7face018520d77c73c
Dmitry Ponyatov <dponiatov@icbcom.ru>
2018-07-03 16:52:52 +0400

    + RPLY
    Signed-off-by: Dmitry Ponyatov <dponiatov@icbcom.ru>

--- PP/vm.py
+++ PP/vm.py
@@ -3,51 +3,61 @@ import sys
 ## data stack
 D = []
 
-## syntax parser by PLY (Python Lex-Yacc) @ http://www.dabeaz.com/ply/
+# ## syntax parser by PLY (Python Lex-Yacc) @ http://www.dabeaz.com/ply/
+# 
+# import ply.lex as lex
+# 
+# ## supported tokens
+# tokens = ['WORD','NUMBER']
+# 
+# ## ignore spaces
+# t_ignore = ' \t\r'
+# 
+# ## line comments can start with # and \
+# ## block comments uses ( )
+# t_ignore_COMMENT = r'[\#\\].*|\(.*\)'
+# 
+# ## new line rule increments line counter in lexer
+# def t_newline(t):
+#     r'\n'
+#     t.lexer.lineno += 1
+#     
+# ## lexer error callback
+# def t_error(t):
+#     raise SyntaxError(t)
+# 
+# ## number parser rule
+# def t_NUMBER(t):
+#     r'[\+\-]?[0-9]+(\.[0-9]*)?'
+#     return t
+# 
+# ## any other non-space groups treated as FORTH word names
+# def t_WORD(t):
+#     r'[A-Za-z0-9_]+'
+#     return t    
+# 
+# ## single lexer instance
+# lexer = lex.lex()
 
-import ply.lex as lex
+from rply import LexerGenerator
 
-## supported tokens
-tokens = ['WORD','NUMBER']
+lexer_generator = LexerGenerator()
 
-## ignore spaces
-t_ignore = ' \t\r'
+lexer_generator.add('NUMBER',r'[\+\-]?[0-9]+(\.[0-9]*)?')
+lexer_generator.add('WORD',r'[A-Za-z0-9_]+')
+lexer_generator.add('SPACE',r'\s+')
 
-## line comments can start with # and \
-## block comments uses ( )
-t_ignore_COMMENT = r'[\#\\].*|\(.*\)'
+lexer = lexer_generator.build()
 
-## new line rule increments line counter in lexer
-def t_newline(t):
-    r'\n'
-    t.lexer.lineno += 1
-    
-## lexer error callback
-def t_error(t):
-    raise SyntaxError(t)
-
-## number parser rule
-def t_NUMBER(t):
-    r'[\+\-]?[0-9]+(\.[0-9]*)?'
-    return t
-
-## any other non-space groups treated as FORTH word names
-def t_WORD(t):
-    r'[A-Za-z0-9_]+'
-    return t    
-
-## single lexer instance
-lexer = lex.lex()
-
-def WORD():
-    token = lexer.token()
-    D.append(token)
-    return token
+def WORD(source):
+    try: token = stream.next() ; D.append(token) ; return token
+    except StopIteration: return None
 
 def INTERPRET(SRC=''):
-    lexer.input(SRC)            # feed lexer with source code
+#     lexer.input(SRC)            # feed lexer with source code
+    source = lexer.lex('1 2.3')
     while True:
-        if not WORD(): break    # break on end of source
+        if not WORD(source): break    # break on end of source
         print D
 
 def main(argv):
